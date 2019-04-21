45f6e4ddd85e09be19ca029ee8bfb186f9f68776
Jokex3 <49209004+Jokex3@users.noreply.github.com>
2019-04-20 20:28:20 -0400

    Operaciones de la Lista
    Contiene la definicion de la Estructura para crear la lista. Además posee las operaciones que se manejara en el main.

---
+++ Operaciones_Lista.h
@@ -0,0 +1,72 @@
+
+typedef struct nodo{
+
+	int tamMaximo;
+	int contadorCantidad;
+	int *arregloDatos;
+
+}ListaNumeros;
+
+
+ListaNumeros *creaListaNumerosVacia(int tamMax){
+	ListaNumeros *ListaAux = malloc(sizeof (struct nodo));
+	ListaAux->tamMaximo = tamMax;
+	ListaAux->contadorCantidad = 0;
+	ListaAux->arregloDatos = malloc(sizeof(int)%tamMax);
+
+
+	return (ListaAux);
+}
+
+/*void insertarAlFinal(ListaNumeros *unaLista,int elemAgregar){
+    if(unaLista->contadorCantidad == 0){
+        unaLista->arregloDatos[unaLista->contadorCantidad] = elemAgregar;
+        unaLista->contadorCantidad++;
+    }else{
+        unaLista->arregloDatos[unaLista->contadorCantidad] = elemAgregar;
+        unaLista->contadorCantidad++;
+    }
+}*/
+
+
+ListaNumeros *creaListaNumerosLlena(int tamMax,int limNumeros){
+	ListaNumeros *ListaAux = creaListaNumerosVacia(tamMax);
+	int cont; int elem;
+	srand(time(NULL));
+	for(cont = 0;cont < tamMax;cont++){
+		elem = rand()%(limNumeros+1);
+		ListaAux->arregloDatos[cont] = elem;
+		//insertarAlFinal(ListaAux,elem);
+        ListaAux->contadorCantidad = cont;
+	}
+
+
+	return (ListaAux);
+}
+
+int eliminaListaNumeros(ListaNumeros *unaListaNumeros){
+	free(unaListaNumeros->arregloDatos); //Elimina primero los datos que ingresamos
+	free(unaListaNumeros);
+
+	return 1; //Retornamos un entero para indicar que se elimino la lista completamente
+}
+
+int buscaNumeroEnListaNumeros(ListaNumeros *unaListaNumeros,int unNumero){
+	int cont;int indice = -1;
+	for(cont = 0;cont < unaListaNumeros->tamMaximo;cont++){
+		if(unaListaNumeros->arregloDatos[cont] == unNumero){
+			indice = cont;
+			break;
+		}
+	}
+
+	return indice;
+}
+
+
+void imprimir(ListaNumeros *unaLista){
+	int cont; printf("\n");
+	for(cont = 0;cont < unaLista->tamMaximo;cont++){
+		printf("[%i]->",unaLista->arregloDatos[cont]);
+	}
+}
